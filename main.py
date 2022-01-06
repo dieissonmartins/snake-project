a6ae21fc0a2015f667e28e0b9f62d3b553c7c342
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-01-06 20:46:21 -0300

    add class by main

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-# teste print
-print("Hello World!")
+# Python3 program to
 
-# teste print foreach
-i = 0
+import datetime
 
-while i < 100:
-    print(i)
-    i += 1
+
+class Main:
+    # attribute
+    today = datetime.datetime.now()
+    input = ''
+
+    def run(self):
+        print("running script---------")
+        print("-----------------------")
+        print(self.today.strftime("%Y-%m-%d"))
+
+        self.input = input("inform date period:\n")
+
+        ret = self.input
+
+        return ret
+
+
+# object instantiation
+execute = Main()
+
+# method through objects
+response = execute.run()
+
+print(response)
+
+print("end process script")
