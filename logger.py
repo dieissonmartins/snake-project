53d5d344149e1ddf150916d0c38b979ebff6100e
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-01-10 22:21:22 -0300

    import loggers

---
+++ logger.py
@@ -0,0 +1,12 @@
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+logging.debug('This message should go to the log file')
+
+logging.info('So should this')
+
+logging.warning('And this, too')
+
+logging.error('And non-ASCII stuff, too, like round and Malmo')
