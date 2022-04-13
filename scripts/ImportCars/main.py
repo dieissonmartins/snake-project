7c0621ef5bb7d7615860ee1751c9fe2518e93385
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-04-13 18:27:40 -0300

    export file csv

--- scripts/ImportCars/main.py
+++ scripts/ImportCars/main.py
@@ -1,9 +1,16 @@
-
 import pandas as pd
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # read the online file by the URL provided above, and assign it to variable "df"
 path = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 
-data = pd.read_csv(path, header = None)
+data = pd.read_csv(path, header=None)
 
+# print values
 print(data.to_string())
+
+# export csv values
+dir_to = dir_path + '/tmp/automobile.csv'
+data.to_csv(dir_to)
