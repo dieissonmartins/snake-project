dc216a91e4b748819f66023ff1bf101ed33c8a2c
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-04-25 20:06:28 -0300

    vars

---
+++ scripts/fundaments/vars.py
@@ -0,0 +1,13 @@
+# int
+var_int = int(100)
+print(var_int)
+
+# float
+var_float = float(100)
+print(var_float)
+
+# bool
+var_bool_01 = True
+var_bool_02 = False
+print(var_bool_01)
+print(var_bool_02)
