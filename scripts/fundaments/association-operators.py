7cce443b4e51fdc1af677b238a66364fb860a8ff
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-05-04 19:16:55 -0300

    commit

--- scripts/fundaments/association-operators.py
+++ scripts/fundaments/association-operators.py
@@ -1,16 +1,9 @@
-# compare operators types
+# association operators types
 
-string_01 = str('Hellow')
-string_02 = str('Hellow2')
+stores = ['Magalu', 'Via', 'Assai', 'Carrefour']
 
-compare_01 = (string_01 is string_01)
-print(compare_01)
+in_via = ('Via' in stores)
+print(in_via)
 
-compare_02 = (string_01 is string_02)
-print(compare_02)
-
-compare_03 = (string_01 == string_02)
-print(compare_03)
-
-compare_04 = (type(string_01) == type(string_02))
-print(compare_04)
+not_in_via = ('Via' not in stores)
+print(not_in_via)
