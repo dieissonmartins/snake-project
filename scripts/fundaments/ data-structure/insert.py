cfec7a9335fa3dd8d68b11dfd425a2906d4ed3a8
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-05-06 08:18:02 -0300

    insert new item
    Signed-off-by: Dieisson Martins <dieisson.martins.santos@gmail.com>

---
+++ scripts/fundaments/ data-structure/insert.py
@@ -0,0 +1,9 @@
+# insert item position
+
+citys = ['Novo Cruzeiro', 'Belo Horizonte', 'São Paulo', 'Betim', 'Contagem', 'Paracicaba']
+
+new_position = len(citys) + 1
+
+citys.insert(new_position, 'New City Insert')
+
+print(citys)
