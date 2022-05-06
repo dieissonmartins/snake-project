84f5b6de0493ac5b0a9785e6794c2dd5f2da5048
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-05-06 08:23:13 -0300

    merge two arrays
    Signed-off-by: Dieisson Martins <dieisson.martins.santos@gmail.com>

---
+++ scripts/fundaments/ data-structure/extends.py
@@ -0,0 +1,9 @@
+# merge two arrays
+
+citys_01 = ['Novo Cruzeiro', 'Belo Horizonte', 'São Paulo', 'Betim', 'Contagem', 'Paracicaba']
+
+citys_02 = ['Itaipe', 'Betim', 'Nova Belem']
+
+new_array_citys = citys_01.extend(citys_02)
+
+print(new_array_citys)
