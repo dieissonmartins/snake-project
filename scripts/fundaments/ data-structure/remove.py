8199b74c412c82d0770d78c95dee48d0b7bba232
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-05-06 08:26:00 -0300

    remove item by name value
    Signed-off-by: Dieisson Martins <dieisson.martins.santos@gmail.com>

---
+++ scripts/fundaments/ data-structure/remove.py
@@ -0,0 +1,8 @@
+# remove item by name value
+
+citys_01 = ['Novo Cruzeiro', 'Belo Horizonte', 'São Paulo', 'Betim', 'Contagem', 'Paracicaba']
+
+city = 'Betim'
+citys_01.remove(city)
+
+print(citys_01)
