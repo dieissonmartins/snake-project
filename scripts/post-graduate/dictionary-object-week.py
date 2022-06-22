e31e63d6f85e0eeadef416492f2c1b82aaf35f46
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-06-22 15:02:19 -0300

    dictionany object week
    Signed-off-by: Dieisson Martins <dieisson.martins.santos@gmail.com>

---
+++ scripts/post-graduate/dictionary-object-week.py
@@ -0,0 +1,17 @@
+weeks = {}
+
+weeks['Sunday'] = []
+weeks['Monday'] = []
+weeks['Tuesday'] = []
+weeks['Wednesday'] = []
+weeks['Thursday'] = []
+weeks['Friday'] = []
+weeks['Saturday'] = []
+
+weeks['Monday'].append({'Português', 'Matemática', 'Geografia', 'Química'})
+weeks['Tuesday'].append({'História'})
+weeks['Wednesday'].append({'Geografia', 'Química'})
+weeks['Thursday'].append({'Física'})
+weeks['Friday'].append({'Inglês', 'Geografia'})
+
+print(weeks)
